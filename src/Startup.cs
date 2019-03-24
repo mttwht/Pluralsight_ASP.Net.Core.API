961f046cef5f8aef25b2d64432c74543a77231bd
Matthew White <Matthew.White02@gmail.com>
2019-03-24 20:46:47 +0000

    Use conventions for versioning

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
+using CoreCodeCamp.Controllers;
 using CoreCodeCamp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc.Versioning.Conventions;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CoreCodeCamp
@@ -21,12 +23,16 @@ namespace CoreCodeCamp
                 options.AssumeDefaultVersionWhenUnspecified = true;
                 options.DefaultApiVersion = new ApiVersion(1, 1);
                 options.ReportApiVersions = true;
-                //options.ApiVersionReader = new QueryStringApiVersionReader(); // this is the default behaviour
-                // // change query string parameter
                 options.ApiVersionReader = ApiVersionReader.Combine(
                     new QueryStringApiVersionReader("v", "version"),
                     new HeaderApiVersionReader("X-Version")
                 );
+
+                options.Conventions.Controller<TalksController>()
+                    .HasApiVersion(new ApiVersion(1, 0))
+                    .HasApiVersion(new ApiVersion(1, 1))
+                    .Action(c => c.Delete(default(string), default(int)))
+                        .MapToApiVersion(1, 1);
             });
 
             services.AddMvc()
