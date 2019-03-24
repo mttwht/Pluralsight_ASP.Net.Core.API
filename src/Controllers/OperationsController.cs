2cbcf00e18cf542bcf90ff826464c3674c229deb
Matthew White <Matthew.White02@gmail.com>
2019-03-24 19:49:47 +0000

    Add a functional API endpoint

---
+++ src/Controllers/OperationsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationsController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public OperationsController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpOptions("reloadconfig")]
+        public ActionResult ReloadConfig()
+        {
+            try {
+                var configRoot = (IConfigurationRoot)configuration;
+                configRoot.Reload();
+                return Ok();
+            }
+            catch(Exception) {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
